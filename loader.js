ed62bb9b1044c2b1d2bdbe80cea3ef1ea9dbd359
peccu <peccul@gmail.com>
2017-12-19 15:51:58 +0900

    initial commit

---
+++ loader.js
@@ -0,0 +1,40 @@
+'use strict';
+const loader = function(queue, cb){
+  var item = queue.shift();
+  var script = document.createElement("script");
+  document.getElementsByTagName("head")[0].appendChild(script);
+  // console.log(src + ' start ' + new Date().getTime());
+  script.onload = function(){
+    // console.log(src + ' loaded ' + new Date().getTime());
+    if(queue.length){
+      return loader(queue, cb);
+    }
+    // console.log('all loaded');
+    return cb();
+  };
+  script.src = item.src + (item.useCache ? '' : "?ts=" + new Date().getTime());
+};
+const isLocal = location.protocol == 'file:';
+const vendorDir = './vendor/js/';
+
+
+const parallel = [
+  {src: (isLocal ? vendorDir : '//cdn.jsdelivr.net/npm/vue@2/dist/') + 'vue.min.js', useCache: true},
+  {src: (isLocal ? vendorDir : '//cdn.jsdelivr.net/npm/jquery@3/dist/') + 'jquery.min.js', useCache: true}
+];
+var finish = parallel.length;
+const queue = [
+  {src: (isLocal ? vendorDir : '//cdn.jsdelivr.net/npm/semantic-ui@2/dist/') + 'semantic.min.js', useCache: true},
+  {src: './links.js', useCache: false},
+  {src: './script.js', useCache: false},
+];
+
+const loadQueue = function(){
+  finish -= 1;
+  if(finish === 0){
+    loader(queue, function(){});
+  }
+};
+parallel.map(function(e, i, a){
+  loader([e], loadQueue);
+});
---
+++ script.js
@@ -0,0 +1,258 @@
+'use strict';
+var intMatch = function(target, obj, regex){
+  return (target && (obj && obj.id.toString().match(regex)));
+};
+
+var nameMatch = function(target, obj, regex){
+  return (target && (obj && obj.name.match(regex)));
+};
+
+var linkFilter = function(query){
+  var regex = new RegExp(query, 'ig');
+  if(query === ''){
+    regex = null;
+  }
+  return function(link){
+    if(!regex){return true;}
+    var result = link.title.match(regex) ||
+        link.description.match(regex) ||
+        link.keyword.reduce(function(acc, e){
+          return acc || e.match(regex);
+        }, false) ||
+        link.category.reduce(function(acc, path){
+          return acc || path.reduce(function(acc2, category){
+            return acc2 || category.match(regex);
+          }, false);
+        }, false);
+    return result;
+  };
+};
+
+var filteredList = function(list, query){
+  return query.split(/ /).reduce(function(acc, e){
+    var filter = linkFilter(e);
+    var filtered = acc.filter(filter);
+    return filtered;
+  }, list);
+};
+
+var zerofill = function(i){
+  return ('00' + (i)).slice(-2);
+};
+
+var ensureNode = function(tree){
+  if(!tree){
+    tree = {
+      contents: [],
+      children: {}
+    };
+  }
+  return tree;
+};
+var addToTree = function(tree, content, path){
+  tree = ensureNode(tree);
+  if(path.length == 0){
+
+    tree.contents.push(content);
+    return;
+  }
+  if(path.length == 1){
+    tree.children[path[0]] = ensureNode(tree.children[path[0]]);
+
+    tree.children[path[0]].contents.push(content);
+    return;
+  }
+  var current = path[0];
+  tree.children[current] = ensureNode(tree.children[current]);
+
+  addToTree(tree.children[current], content, path.slice(1));
+};
+
+const generateTree = function(links){
+  var data = {
+    contents: [],
+    children: {}
+  };
+  links.map(function(e, i, a){
+    e.category.map(function(path){
+      addToTree(data, e, path);
+    });
+  });
+  return data;
+};
+
+// define the item component
+Vue.component('itemOrig', {
+  template: '#item-template-orig',
+  props: {
+    model: Object
+  },
+  data: function () {
+    return {
+      open: true
+    };
+  },
+  computed: {
+    isFolder: function () {
+      return this.model.children &&
+        this.model.children.length;
+    }
+  },
+  methods: {
+    toggle: function () {
+      if (this.isFolder) {
+        this.open = !this.open;
+      }
+    },
+    changeType: function () {
+      return;
+      if (!this.isFolder) {
+        Vue.set(this.model, 'children', []);
+        this.addChild();
+        this.open = true;
+      }
+    },
+    addChild: function () {
+      this.model.children.push({
+        name: 'new stuff'
+      });
+    }
+  }
+});
+
+// define the item component
+Vue.component('item', {
+  template: '#item-template',
+  props: {
+    category: String,
+    query: String,
+    setquery: Function,
+    model: Object
+  },
+  data: function () {
+    return {
+      // default collapse/open
+      open: true
+    };
+  },
+  computed: {
+    filtered: function () {
+      return filteredList(this.model.contents, this.query);
+    },
+    isFolder: function () {
+      return (this.model.children &&
+              Object.keys(this.model.children).length) ||
+        (this.model.contents &&
+         this.model.contents.length);
+    },
+    isOpen: function () {
+      return this.open || this.query !== '';
+    }
+  },
+  methods: {
+    toggle: function () {
+      if (this.isFolder) {
+        this.open = !this.open;
+      }
+    },
+    changeType: function () {
+      return;
+      if (!this.isFolder) {
+        Vue.set(this.model, 'children', []);
+        this.addChild();
+        this.open = true;
+      }
+    },
+    addChild: function () {
+      this.model.children.push({
+        name: 'new stuff'
+      });
+    }
+  }
+});
+
+// define the item component
+Vue.component('itemContent', {
+  template: '#content-template',
+  props: {
+    link: Object,
+    query: String,
+    setquery: Function
+  },
+  methods: {
+    filter: function(link){
+      return this.query.split(/ /).reduce(function(acc, e){
+        var filter = linkFilter(e);
+        return acc || filter(link);
+      }, false);
+    },
+    addTag: function(tag){
+      if(this.query === ''){
+        this.query = tag;
+        this.setquery(this.query);
+        return;
+      }
+      this.query += ' ' + tag;
+      this.setquery(this.query);
+    },
+    isActive: function(active){
+      let today = new Date();
+      let from = !active.from || (new Date(active.from)) <= today;
+      let to = !active.to || today <= (new Date(active.to));
+      return from && to;
+    },
+    ymd: function(date){
+      let d = new Date(date);
+      return d.getFullYear() + '/'
+        + zerofill(d.getMonth() + 1) + '/'
+        + zerofill(d.getDate());
+    }
+  }
+});
+
+const mountpoint = 'app';
+var app = new Vue({
+  el: '#' + mountpoint,
+  props: [
+  ],
+  components: {
+  },
+  computed: {
+    filtered: function () {
+      return filteredList(this.links, this.query);
+    }
+  },
+  data: {
+    fetched: false,
+    query: '',
+    links: window.links,
+    windows: window.windows,
+    treeData: generateTree(window.links),
+    category: 'リンク集'
+  },
+  watch: {
+  },
+  beforeCreate: function(){
+  },
+  created: function(){
+  },
+  beforeMount: function(){
+  },
+  mounted: function(){
+    document.getElementById(mountpoint).classList.remove('loading');
+    $('.menu .item').tab();
+  },
+  beforeUpdate: function(){
+  },
+  updated: function(){
+  },
+  beforeDestroy: function(){
+  },
+  destroyed: function(){
+  },
+  methods: {
+    setquery: function(query){
+      this.query = query;
+    }
+  }
+});
