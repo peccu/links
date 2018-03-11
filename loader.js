511a9ad6132c5732613733e386519d43a1e0159c
peccu <peccul@gmail.com>
2018-03-11 23:32:39 +0900

    update loader to support to specify load path

--- loader.js
+++ loader.js
@@ -15,27 +15,29 @@ const loader = function(queue, cb){
   script.src = item.src + (item.useCache ? '' : "?ts=" + new Date().getTime());
 };
 const isLocal = location.protocol == 'file:';
-const vendorDir = './vendor/js/';
+const vendorDir = 'vendor/js/';
 
-const parallel = [
-  {src: (isLocal ? vendorDir : '//cdn.jsdelivr.net/npm/vue@2/dist/') + 'vue.min.js', useCache: true},
-  {src: (isLocal ? vendorDir : '//cdn.jsdelivr.net/npm/remotestoragejs@1.0.2/release/') + 'remotestorage.min.js', useCache: true},
-  {src: (isLocal ? vendorDir : '//cdn.jsdelivr.net/npm/remotestorage-widget@1.3.0/build/') + 'widget.min.js', useCache: true},
-  {src: (isLocal ? vendorDir : '//cdn.jsdelivr.net/npm/jquery@3/dist/') + 'jquery.min.js', useCache: true}
-];
-var finish = parallel.length;
-const queue = [
-  {src: (isLocal ? vendorDir : '//cdn.jsdelivr.net/npm/semantic-ui@2/dist/') + 'semantic.min.js', useCache: true},
-  {src: './rs.js', useCache: false},
-  {src: './script.js', useCache: false},
-];
+const loadFrom = function(from){
+  const parallel = [
+    {src: (isLocal ? from + vendorDir : '//cdn.jsdelivr.net/npm/vue@2/dist/') + 'vue.min.js', useCache: true},
+    {src: (isLocal ? from + vendorDir : '//cdn.jsdelivr.net/npm/remotestoragejs@1.0.2/release/') + 'remotestorage.min.js', useCache: true},
+    {src: (isLocal ? from + vendorDir : '//cdn.jsdelivr.net/npm/remotestorage-widget@1.3.0/build/') + 'widget.min.js', useCache: true},
+    {src: (isLocal ? from + vendorDir : '//cdn.jsdelivr.net/npm/jquery@3/dist/') + 'jquery.min.js', useCache: true}
+  ];
+  var finish = parallel.length;
+  const queue = [
+    {src: (isLocal ? from + vendorDir : '//cdn.jsdelivr.net/npm/semantic-ui@2/dist/') + 'semantic.min.js', useCache: true},
+    {src: from + 'rs.js', useCache: false},
+    {src: from + 'script.js', useCache: false},
+  ];
 
-const loadQueue = function(){
-  finish -= 1;
-  if(finish === 0){
-    loader(queue, function(){});
-  }
+  const loadQueue = function(){
+    finish -= 1;
+    if(finish === 0){
+      loader(queue, function(){});
+    }
+  };
+  parallel.map(function(e, i, a){
+    loader([e], loadQueue);
+  });
 };
-parallel.map(function(e, i, a){
-  loader([e], loadQueue);
-});
