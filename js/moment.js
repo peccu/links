5491e8ae7c7a11a348e0a164378d52cffa6fa91b
peccu <peccul@gmail.com>
2018-04-19 11:05:23 +0900

    add seconds to moment#calendar format

--- js/moment.js
+++ js/moment.js
@@ -1,11 +1,11 @@
 'use strict';
 moment.updateLocale('en', {
   calendar : {
-    lastDay : '[Yesterday at] HH:mm',
-    sameDay : '[Today at] HH:mm',
-    nextDay : '[Tomorrow at] HH:mm',
-    lastWeek : '[last] dddd [at] HH:mm',
-    nextWeek : 'dddd [at] HH:mm',
+    lastDay : '[Yesterday at] HH:mm:ss',
+    sameDay : '[Today at] HH:mm:ss',
+    nextDay : '[Tomorrow at] HH:mm:ss',
+    lastWeek : '[last] dddd [at] HH:mm:ss',
+    nextWeek : 'dddd [at] HH:mm:ss',
     sameElse : 'YYYY/MM/DD'
   }
 });
